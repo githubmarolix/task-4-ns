4898cf6d1f577cbaf45632eace3c89e217c2441f
githubmarolix <130967309+githubmarolix@users.noreply.github.com>
2023-08-09 17:55:38 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,16 +1,16 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Change the database connection URL with yours
-mongoose.connect('mongodb+srv://tatareddymarolix:a1PJcq9yKDsRoSeq@cluster0.ptzqraw.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const uri = 'mongodb+srv://tatareddymarolix:a1pjcq9ykdsroseq@cluster0.ptzqraw.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
-  console.log('Connected to MongoDB successfully!');
+    console.log('Connected to MongoDB successfully!');
 }).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
+    console.error('Error connecting to MongoDB:', error.message);
 });
 
 module.exports = { mongoose };
-
