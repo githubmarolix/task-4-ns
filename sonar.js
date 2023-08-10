7caa02b9d8567cb96fefc7a7cdc41b92905027d0
githubmarolix <130967309+githubmarolix@users.noreply.github.com>
2023-08-10 20:08:54 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -1,1 +1,12 @@
-hi this is ktr
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://54.163.86.60:9000',
+  options: {
+    'sonar.sources': './',
+    'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
+    'sonar.login': 'your-sonarqube-login-token',
+    'sonar.projectKey': 'nhs-app',
+    'sonar.projectName': 'NHS App',
+  },
+}, () => {});
