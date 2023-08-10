46a3687b991015bd2d06f2db28a8d88b0cddc9db
githubmarolix <130967309+githubmarolix@users.noreply.github.com>
2023-08-10 20:41:39 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -4,7 +4,6 @@ sonarqubeScanner({
   serverUrl: 'http://54.163.86.60:9000',
   options: {
     'sonar.login': 'fb464caca0058c777645e903c868262699a2cbf1',
-    'sonar.password': 'admin',
     'sonar.sources': './',
     'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
     'sonar.login': 'your-sonarqube-login-token',
