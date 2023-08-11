95d6d7eff11ac5b9674b041c8af6039ac2cbdf20
githubmarolix <130967309+githubmarolix@users.noreply.github.com>
2023-08-11 16:04:34 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.87.239.204:9000/projects',
+  serverUrl: 'http://3.87.239.204:9000',
   options: {
     'sonar.login': '5afcca0c3955fae6fd92a83eece58ae842071d1a',
     'sonar.sources': './',
